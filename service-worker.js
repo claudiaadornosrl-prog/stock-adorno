b15bf872c6350d46b8947edc95ffc54060d487e6
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-07-27 12:04:16 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Stock Adorno
-const CACHE_VERSION = 'stock-adorno-v18-icono-espalma';
+const CACHE_VERSION = 'stock-adorno-v19-ocultar-sin-stock-espalma';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
