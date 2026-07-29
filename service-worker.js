2cd8107f5a4ebc0f8a97dbe8eac9cf8330948bba
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-07-29 13:05:40 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Stock Adorno
-const CACHE_VERSION = 'stock-adorno-v32-kebab-listas';
+const CACHE_VERSION = 'stock-adorno-v33-menu-limpio';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
