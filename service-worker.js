490c319ed59c40824312779b85754ae5de7ad644
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-08-07 13:28:24 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Stock Adorno
-const CACHE_VERSION = 'stock-adorno-v46-icono-pwa';
+const CACHE_VERSION = 'stock-adorno-v48-push-endpoint-app';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', e => {
